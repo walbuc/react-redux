eb985d10373f74f48d8de964544642532c01e7f7
walter <buczacka.walter@gmail.com>
2018-10-09 15:23:31 -0300

    add pilots and mechs

--- src/features/unitInfo/UnitInfo.js
+++ src/features/unitInfo/UnitInfo.js
@@ -1,30 +1,64 @@
-import React from "react";
-import {Form, Dropdown, Segment} from "semantic-ui-react";
+import { connect } from "react-redux";
+import React, { Component } from "react";
+import { Form, Dropdown, Segment } from "semantic-ui-react";
+import { selectUnitInfo } from "./unitInfoSelectors";
+import * as actions from "./unitInfoActions";
+import {getValueFromEvent} from "common/utils/clientUtils";
 
 const FACTIONS = [
-    {value : "cc", text : "Capellan Confederation"},
-    {value : "dc", text : "Draconis Combine"},
-    {value : "fs", text : "Federated Suns"},
-    {value : "fwl", text : "Free Worlds League"},
-    {value : "lc", text : "Lyran Commonwealth"},
+  { value: "cc", text: "Capellan Confederation" },
+  { value: "dc", text: "Draconis Combine" },
+  { value: "fs", text: "Federated Suns" },
+  { value: "fwl", text: "Free Worlds League" },
+  { value: "lc", text: "Lyran Commonwealth" },
+  { value: "wd", text: "Wolf's Dragoons" }
 ];
 
-const UnitInfo = () => (
-    <Segment attached="bottom">
-        <Form size="large">
-            <Form.Field name="name" width={6} >
-                <label>Unit Name</label>
-                <input placeholder="Name" />
-            </Form.Field>
-            <Form.Field name="affiliation" width={6}>
-                <label>Affiliation</label>
-                <Dropdown
-                    selection
-                    options={FACTIONS}
-                />
-            </Form.Field>
-        </Form>
-    </Segment>
-);
+const mapState = state => ({
+  unitInfo: selectUnitInfo(state)
+});
 
-export default UnitInfo;
+class UnitInfo extends Component {
+  onAffiliationChanged = (e, result) => {
+    const {name, value} = result;
+    const newValues = { [name] : value};
+    this.props.updateUnitInfo(newValues);
+  };
+
+  onNameChanged = e => {
+    const newValues = getValueFromEvent(e);
+    this.props.updateUnitInfo(newValues);
+  };
+
+  render() {
+    const { unitInfo = {} } = this.props;
+    const { name, affiliation } = unitInfo;
+    return (
+      <Segment attached="bottom">
+        <Form size="large">
+          <Form.Field name="name" width={6}>
+            <label>Unit Name</label>
+            <input
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={this.onNameChanged}
+            />
+          </Form.Field>
+          <Form.Field name="affiliation" width={6}>
+            <label>Affiliation</label>
+            <Dropdown
+              name="affiliation"
+              selection
+              options={FACTIONS}
+              value={affiliation}
+              onChange={this.onAffiliationChanged}
+            />
+          </Form.Field>
+        </Form>
+      </Segment>
+    );
+  }
+}
+
+export default connect(mapState, actions)(UnitInfo);
