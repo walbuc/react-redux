8dc60d1dbab59dc66f234f3b5733cf283ef5a476
walter <buczacka.walter@gmail.com>
2018-10-19 08:42:07 -0300

    add editting entities logic

---
+++ src/common/utils/modelUtils.js
@@ -0,0 +1,7 @@
+
+//utility to simplify lookup
+export function getModelByType(session, itemType, itemID) {
+     const modelClass = session[itemType];
+     const model = modelClass.withId(itemID);
+     return model;
+ }
---
+++ src/features/entities/entitySelectors.js
@@ -0,0 +1,9 @@
+import { createSelector } from "reselect";
+
+import orm from "app/orm";
+
+export const selectEntities = state => state.entities;
+
+export const getEntitiesSession = createSelector(selectEntities, entities =>
+  orm.session(entities)
+);
