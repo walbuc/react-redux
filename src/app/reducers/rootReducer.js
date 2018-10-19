3b7f8da3fff6a990c7adf9813fc3158f6efa0bac
walter <buczacka.walter@gmail.com>
2018-10-19 08:40:52 -0300

    add editing entities

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,17 +2,21 @@ import {combineReducers} from "redux";
 import {reduceReducers} from "common/utils/reducerUtils";
 
 import entitiesReducer from "./entitiesReducer";
+import editingEntitiesReducer from "./editingEntitiesReducer";
 import tabReducer from "features/tabs/tabsReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 import pilotsReducer from "features/pilots/pilotsReducer";
 import mechsReducer from "features/mechs/mechsReducer";
 
+
 import entityCrudReducer from "features/entities/entityReducer";
+import editingFeatureReducer from "features/editing/editingReducer";
 
 const combinedReducer = combineReducers({
     unitInfo : unitInfoReducer,
     tabs : tabReducer,
     entities : entitiesReducer,
+    editingEntities : editingEntitiesReducer,
     pilots : pilotsReducer,
     mechs : mechsReducer
 });
@@ -20,5 +24,6 @@ const combinedReducer = combineReducers({
 const rootReducer = reduceReducers(
     combinedReducer,
     entityCrudReducer,
+    editingFeatureReducer,
 );
 export default rootReducer;
